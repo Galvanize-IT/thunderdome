09cd6d4c6f3e03656413cbbd892b0cce22589c95
Aaron O. Ellis <aaronoellis@gmail.com>
2015-05-21 11:17:16 -0600

    Added jquery and bootstrap js dependencies with a gulp concat command

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
 var gls = require('gulp-live-server');
 
 // A reason to hate gulp: https://github.com/gulpjs/gulp/issues/259
@@ -8,6 +9,16 @@ function handleError(err) {
   console.log(err.toString());
   this.emit('end');
 }
+
+// concat the js dependencies
+gulp.task('js', function() {
+  return gulp.src([
+      './node_modules/jquery/dist/jquery.min.js',
+      './node_modules/bootstrap/dist/js/bootstrap.min.js'
+    ])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./dist'));
+});
 
 // Re-compile changes to Less files
 gulp.task('less', function() {
@@ -23,4 +34,4 @@ gulp.task('watch', function() {
   gulp.watch(['./index.html', './dist/app.css'], server.notify);
 });
 
-gulp.task('default', ['less', 'watch']);
+gulp.task('default', ['js', 'less', 'watch']);
