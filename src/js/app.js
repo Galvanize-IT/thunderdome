5b08c4cf717bf1c4bb698c3fc0fdfb986bb60000
Aaron O. Ellis <aaronoellis@gmail.com>
2015-12-27 00:42:20 -0500

    Basic integration of Backbone and React

--- src/js/app.js
+++ src/js/app.js
@@ -6,35 +6,48 @@
 
 var Row = React.createClass({
   getInitialState: function() {
-    return this.props;
+    return this.props.item;
+  },
+  upQuantity: function() {
+    this.setState({quantity: this.state.quantity + 1});
   },
   render: function() {
-    return <li>{this.state.item.name} ({this.state.item.quantity})</li>;
+    return (<li>
+      <span>{this.state.name} ({this.state.quantity})</span>
+      <button onClick={this.upQuantity}>+</button>
+    </li>);
   }
 });
 
 var List = React.createClass({
+  update: function() {
+    console.log('updating')
+    this.setState({items: this.props.items.toJSON()});
+  },
   getInitialState: function() {
-    return {init: this.props.items, items: this.props.items};
+    this.props.items.on('add', this.update, this);
+    return {items: this.props.items.toJSON()};
   },
   addItem: function() {
+    console.log('add item');
+    this.props.items.add({name: 'Huh', quantity: 1});
     // New items must be added to init
-    this.setState({
-      items: this.state.items.concat([{name: 'Whatever', quantity: 0}])
-    });
+    // this.setState({
+    //   items: this.state.items.concat([{name: 'Whatever', quantity: 0}])
+    // });
   },
   search: function(e) {
     var value = e.target.value;
     // If no search term is given, skip the filter
     if (!value) {
-      this.setState({items: this.state.init});
+      this.setState({items: this.state.init.toJSON()});
       return;
     }
 
     var sanitizedValue = value.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
     // Use a regex to quickly perform a case-insensitive match
     var re = new RegExp(sanitizedValue, 'i');
-    var items = this.state.init.filter(function(item) {
+    var items = this.state.init.toJSON().filter(function(item) {
         return re.test(item.name);
     });
     this.setState({items: items});
@@ -46,7 +59,7 @@ var List = React.createClass({
       <button onClick={this.addItem}>Add</button>
       <ul>
         {this.state.items.map(function(item) {
-          return <Row key={item.name} item={item}/>;
+          return <Row key={item.id} item={item}/>;
         }.bind(this))}
       </ul>
     </div>
@@ -62,13 +75,16 @@ var List = React.createClass({
 // })
 
 var items = [
-  {name: 'Lamp', quantity: 2},
-  {name: 'Chair', quantity: 12},
-  {name: 'Desk', quantity: 2},
-  {name: 'Drawer', quantity: 1}
+  {id: 2, name: 'Chair', quantity: 12},
+  {id: 3, name: 'Desk', quantity: 2},
+  {id: 4, name: 'Drawer', quantity: 1},
+  {id: 1, name: 'Lamp', quantity: 2}
 ];
 
+var Items = Backbone.Collection.extend({});
+
 $(function() {
-  ReactDOM.render(<List items={items}/>, $('body')[0]);
-  // ReactDOM.render(<Row item={items[0]}/>, $('body')[0]);
+  var list = new Items(items);
+  list.on('change', function() {});
+  ReactDOM.render(<List items={list}/>, $('body')[0]);
 });
